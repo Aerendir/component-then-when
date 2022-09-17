aec047c15011af7e0247e176f74e96f16afbd628
Aerendir <hello@aerendir.me>
2022-09-17 13:13:23 +0200

    Update baselines.

--- .baseline-phan.php
+++ .baseline-phan.php
@@ -10,6 +10,7 @@
 return [
     // # Issue statistics:
     // PhanUnreferencedPublicMethod : 7 occurrences
+    // PhanDeprecatedFunction : 6 occurrences
     // PhanUnreferencedPublicClassConstant : 4 occurrences
     // PhanTypeMismatchArgumentNullable : 2 occurrences
     // PhanUndeclaredStaticMethod : 2 occurrences
@@ -23,15 +24,15 @@ return [
 
     // Currently, file_suppressions and directory_suppressions are the only supported suppressions
     'file_suppressions' => [
-        'src/RetryStrategyBuilder.php' => ['PhanTypeMismatchDeclaredReturn', 'PhanUnreferencedPublicMethod'],
-        'src/Strategy/AbstractStrategy.php' => ['PhanUndeclaredConstantOfClass', 'PhanUnreferencedProtectedMethod', 'PhanUnreferencedPublicMethod'],
+        'src/RetryStrategyBuilder.php' => ['PhanDeprecatedFunction', 'PhanTypeMismatchDeclaredReturn', 'PhanUnreferencedPublicMethod'],
+        'src/Strategy/AbstractStrategy.php' => ['PhanDeprecatedFunction', 'PhanUndeclaredConstantOfClass', 'PhanUnreferencedProtectedMethod', 'PhanUnreferencedPublicMethod'],
         'src/Strategy/ConstantStrategy.php' => ['PhanUnreferencedPublicClassConstant'],
         'src/Strategy/ExponentialStrategy.php' => ['PhanUnreferencedPublicClassConstant'],
         'src/Strategy/LinearStrategy.php' => ['PhanUnreferencedPublicClassConstant'],
         'src/Strategy/NeverRetryStrategy.php' => ['PhanUnreferencedPublicClassConstant'],
         'src/Strategy/TimeFixedStrategy.php' => ['PhanTypeMismatchArgumentNullable', 'PhanUndeclaredMethod'],
         'src/ThenWhen.php' => ['PhanUnreferencedClass', 'PhanUnreferencedPublicMethod'],
-        'src/TryAgain.php' => ['PhanPossiblyInfiniteRecursionSameParams', 'PhanUnreferencedPublicMethod'],
+        'src/TryAgain.php' => ['PhanDeprecatedFunction', 'PhanPossiblyInfiniteRecursionSameParams', 'PhanUnreferencedPublicMethod'],
         'tests/Strategy/NeverRetryStrategyTest.php' => ['PhanRedefinedExtendedClass', 'PhanUndeclaredStaticMethod', 'PhanUnreferencedClass', 'PhanUnreferencedPublicMethod'],
     ],
     // 'directory_suppressions' => ['src/directory_name' => ['PhanIssueName1', 'PhanIssueName2']] can be manually added if needed.
